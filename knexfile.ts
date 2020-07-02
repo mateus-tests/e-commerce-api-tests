cbe0a2afa94f66a155fefbea817de2058478e89a
Computrailer <letsgood56@gmail.com>
2020-07-02 14:58:42 -0300

    Database changed

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    client : 'sqlite3',
-    connection : {
-        filename : path.resolve(__dirname, 'src','database', 'database.sqlite'), 
-    },
+    client : 'pg',
+    connection : process.env.DATABASE_URL,
     migrations : {
         directory : path.resolve(__dirname, 'src', 'database', 'migrations')
     },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,9 @@
 import knex from 'knex';
-import path from 'path';
 
 
 const connection = knex({
-    client : 'sqlite3',
-    connection : {
-        filename : path.resolve(__dirname, 'database.sqlite'), 
-    },
+    client : 'pg',
+    connection : process.env.DATABASE_URL,
     useNullAsDefault : true
 });
 
